c0f82c3a7d4b5156d78029ae637d89a3862ac30c
suraj1716 <43661849+suraj1716@users.noreply.github.com>
2023-04-25 13:26:25 +1000

    Add files via upload

---
+++ BiWeekly_4/Vat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Vat
+    {
+        double v_perc = 0.03;
+        double Total_Vat;
+        
+      
+
+    }
+}
